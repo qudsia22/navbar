3ff63fb41c8aa5d57baa845a7df4d3256b684fe4
qudsia22 <80464598+qudsia22@users.noreply.github.com>
2021-08-03 22:59:32 +0500

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems=[
+    {
+    title: "Book Trip",
+    url:'#',
+    cName:'nav-links'
+    },
+    {
+    title: "Book Hotel",
+    url:'#',
+    cName:'nav-links'
+    },
+    {
+    title: "Blog",
+    url:'#',
+    cName:'nav-links'
+    },
+    {
+        title: "Login",
+        url:'#',
+        cName:'nav-links'
+        },
+        {
+            title: 'start business',
+            url: '#',
+            cName:'nav-links-mobile'
+        },
+    ]
